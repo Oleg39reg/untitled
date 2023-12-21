a9a259638a06549c8ef94f6ed08ec1dac3287148
Oleg <olegkuz39@gmail.com>
2023-12-21 16:41:09 +0200

    Alishev_lessons

---
+++ src/Lesson_2_Variable.java
@@ -0,0 +1,20 @@
+public class Lesson_2_Variable {
+    //   Тема: Переменные
+    public static void main(String[] args) {
+//        Целочисленный тип данных
+        byte myByte = 123;
+        short myShort = 3244;
+        int myInt = 587857;
+        long myLong = 312354123;
+
+//        Вещественный тип данных
+        double myDouble = 234.4; //64 бита
+        float myFloat = 4234.3f; //32 бита
+
+//        Символьный тип данных
+        char myChar = '&'; //символы из unicod
+
+        //Логический тип данных
+        boolean myBollean = true; // false
+    }
+}
